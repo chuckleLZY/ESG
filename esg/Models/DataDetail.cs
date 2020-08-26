cd298d0d58447fe753f76165bff374bfa58783a0
1851759 <49675260+1851759@users.noreply.github.com>
2020-08-26 15:22:28 +0800

    Add files via upload

---
+++ esg/Models/DataDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace esg.Models
+{
+    public class DataDetail
+    {
+        public String ESG_Id { get; set; }
+        public int Type { get; set; }
+    }
+}
